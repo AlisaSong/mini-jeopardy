7c678f438f654467273af6112e1bee5a78854832
Alisa Song <alisasong@gmail.com>
2020-01-25 21:59:55 -0800

    Add project skeleton: components, services and model.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './components/board/board.component';
+import { InteractionComponent } from './components/interaction/interaction.component';
+import { ScorekeeperComponent } from './components/scorekeeper/scorekeeper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BoardComponent,
+    InteractionComponent,
+    ScorekeeperComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/scorekeeper/scorekeeper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scorekeeper',
+  templateUrl: './scorekeeper.component.html',
+  styleUrls: ['./scorekeeper.component.scss']
+})
+export class ScorekeeperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
